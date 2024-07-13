accc3a9ff6fdf5ca5d9d93c024c9de6f59ce48b5
AdityaRajBoum <adityar_ug_22@ece.nits.ac.in>
2024-07-13 10:38:35 +0530

    day 1 variables

---
+++ 01-variables.js
@@ -0,0 +1,42 @@
+//task 1
+var num = 54;
+console.log(num);
+//task 2
+let word = "char";
+console.log(word);
+//task 3
+const bool = true;
+console.log(bool);
+
+//task 4
+const number = 54;
+console.log(typeof(number));
+
+const word2 = "hello";
+console.log(typeof(word2));
+
+const boolean = true;
+console.log(typeof(boolean));
+
+const object = {
+    name : "aditya ",
+    age : ' 21',
+}
+console.log(typeof(object));
+
+
+const array = ['a', 'b', 'c', 1,3, 6,];
+console.log(typeof(array));
+
+//task 5
+let val =5;
+console.log(val);
+
+val = 'hello';
+console.log(val);
+
+//task 6
+const value = 6;
+// value =7;
+console.log(value);
+
